8c2b199e1b5c259f3419859989c1887aa3601785
Michiel Idema <m.idema@textinfo.nl>
2022-06-21 22:41:50 +0200

    backend for duplicate serial numbers check implemented.

--- content.js
+++ content.js
@@ -4,7 +4,20 @@ chrome.runtime.onMessage.addListener(receiveMessage)
 
 function receiveMessage(msg, sender, sendResponse){
 
-    var allElements = document.getElementsByTagName("*");
+    console.log(msg.type)
+
+    switch(msg.type){
+        case "paste_model_version":
+            pasteModelVersion(msg) 
+            break
+        case "check_request":
+            checkSerialNumbers();    
+            break
+    }
+}
+
+function pasteModelVersion(msg){
+    var allElements = document.getElementsByTagName("*")
 
     for (var i=0, max=allElements.length; i < max; i++) {
 
@@ -12,4 +25,33 @@ function receiveMessage(msg, sender, sendResponse){
             allElements[i].value = msg.txt
         }
     }
+}
+
+function checkSerialNumbers(){
+    var allElements = document.getElementsByTagName("*")
+    var serialNumbers = []
+
+    for (var i=0, max=allElements.length; i < max; i++) {
+        if(allElements[i].id === "serial"){
+            serialNumbers.push(allElements[i].value)
+        }
+    }
+
+    console.log(serialNumbers)
+
+    if(containsDuplicates(serialNumbers)){
+        console.log("duplicates found!")
+    }  
+}
+
+function containsDuplicates(array) {
+    const result = array.some(element => {
+      if (array.indexOf(element) !== array.lastIndexOf(element) && element !== '') { 
+        return true 
+      }
+
+      return false
+    })
+  
+    return result
 }
--- popup.js
+++ popup.js
@@ -3,6 +3,13 @@ document.getElementById("button").addEventListener("click", () => {
     let input = document.getElementById("input_num").value
 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {txt: input})
+        chrome.tabs.sendMessage(tabs[0].id, {type: "paste_model_version", txt: input})
+    })
+})
+
+
+document.getElementById("check_button").addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: "check_request"})
     })
 })

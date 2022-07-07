4a2431b06d426c1425427accf7e602ce011885bc
Michiel Idema <m.idema@textinfo.nl>
2022-07-07 22:30:32 +0200

    fixed messaging to new created tab bug.

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 console.log("background script hello")
 
+var queue = []
+
 chrome.runtime.onMessage.addListener(receiveMessage)
 
 function receiveMessage(msg, sender, sendResponse){
@@ -11,18 +13,21 @@ function receiveMessage(msg, sender, sendResponse){
 }
 
 function setupMsgToImagingHelper(msg){
-    chrome.tabs.sendMessage(msg.imagingHelperTabData.id, 
-        { 
-            type: "imaging_helper_setup_to_imaging_helper_tab",
-            serialInfo: msg.serialInfo
-        }
-    )    
+    queue.push({ tabID: msg.imagingHelperTabData.id, serialInfo: msg.serialInfo })  
 }
 
-//hier was ik
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
     console.log(info)
     if (info.status === 'complete') {
-        console.log("tab:"+tabId+"complete")
+        for(var i = 0; i < queue.length; i++){
+            if(queue[i].tabID == tabId){
+                chrome.tabs.sendMessage(tabId, 
+                    { 
+                        type: "imaging_helper_setup_to_imaging_helper_tab",
+                        serialInfo: queue[i].serialInfo
+                    }
+                )                 
+            }
+        }   
     }
 });

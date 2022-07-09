047227821180a44e7f712d071aeeec54cf07fd16
Michiel Idema <m.idema@textinfo.nl>
2022-07-09 17:31:05 +0200

    added mvp html to imaging helper

--- imaging_helper.js
+++ imaging_helper.js
@@ -1,16 +1,11 @@
-console.log("hellooooo")
-
 chrome.runtime.onMessage.addListener(receiveMessage)
 
 function receiveMessage(msg, sender, sendResponse){
 
-    console.log("imaging helper received message!!!!")
-    console.log(msg)
-
     switch(msg.type){
         case "setup_data":
-            console.log(msg)
             setup(msg.serialInfo)
+            console.log(msg.serialInfo)
             break
     }
 }
@@ -18,6 +13,29 @@ function receiveMessage(msg, sender, sendResponse){
 function setup(serialInfo){
     for(var i = 0; i < serialInfo.length; i++){
         laptopInfoDiv = document.getElementById("laptop-info")
-        laptopInfoDiv.innerHTML += "<div>"+serialInfo[i].signpostLabel+"</div>"
+
+        laptopInfoDiv.innerHTML += "".concat(
+            "<div>", serialInfo[i].signpostLabel, 
+                "<div id='checkmarks'>",
+                    buildCheckmarkDiv(serialInfo[i].checkMarks.scriptingData),
+                    buildCheckmarkDiv(serialInfo[i].checkMarks.synergyId),
+                    buildCheckmarkDiv(serialInfo[i].checkMarks.intune),
+                    buildCheckmarkDiv(serialInfo[i].checkMarks.specifications),
+                    buildCheckmarkDiv(serialInfo[i].checkMarks.decommisioned),
+                "</div>",
+            "</div>"    
+        )
     }
+}
+
+function buildCheckmarkDiv(markStatus){
+    var imagePath = ""
+
+    if(markStatus){
+        imagePath = "images/checkmark.svg.png"
+    } else { 
+        imagePath = "images/exclamation_triangle.jpg"
+    }
+
+    return "".concat("<div><img src='", imagePath,"' width='25' height='25' alt='failed to load image'></div>")
 }

8a18e8d9db34ccdcaa474c9ae372f24d9337e1e2
Michiel Idema <m.idema@textinfo.nl>
2022-07-09 14:01:20 +0200

    little background.js refactor

--- background.js
+++ background.js
@@ -16,6 +16,9 @@ function setupMsgToImagingHelper(msg){
     imagingHelperTabsInfo.push({ tabID: msg.imagingHelperTabData.id, serialInfo: msg.serialInfo })  
 }
 
+// todo verander dataflow structuur zodat de serialInfo pas requested wordt wanneer de tab complete is. 
+// Nu kan de tab complete event soms eerder klaar zijn dan dat de content script zijn message naar de background 
+// heeft gestuurd. Dan krijg je die ene error weer
 chrome.tabs.onUpdated.addListener(function (tabId , info) {
     if(info.status !== 'complete'){ return }
 
@@ -29,16 +32,4 @@ chrome.tabs.onUpdated.addListener(function (tabId , info) {
             )                 
         }
     }   
-    // if (info.status === 'complete') {
-    //     for(var i = 0; i < queue.length; i++){
-    //         if(queue[i].tabID == tabId){
-    //             chrome.tabs.sendMessage(tabId, 
-    //                 { 
-    //                     type: "imaging_helper_setup_to_imaging_helper_tab",
-    //                     serialInfo: queue[i].serialInfo
-    //                 }
-    //             )                 
-    //         }
-    //     }   
-    // }
 });
--- imaging_helper.js
+++ imaging_helper.js
@@ -9,6 +9,14 @@ function receiveMessage(msg, sender, sendResponse){
     switch(msg.type){
         case "imaging_helper_setup_to_imaging_helper_tab":
             console.log(msg)
+            setup(msg.serialInfo)
             break
+    }
+}
+
+function setup(serialInfo){
+    for(var i = 0; i < serialInfo.length; i++){
+        laptopInfoDiv = document.getElementById("laptop-info")
+        laptopInfoDiv.innerHTML += "<div>"+serialInfo[i].signpostLabel+"</div>"
     }
 }

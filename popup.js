39900bd2329b8c9fd31e8f87399f369463f85841
Michiel Idema <m.idema@textinfo.nl>
2022-08-23 22:39:26 +0200

    fixed merging bug.

--- popup.js
+++ popup.js
@@ -13,12 +13,6 @@ document.getElementById("button").addEventListener("click", () => {
     })
 })
 
-document.getElementById("check_button").addEventListener("click", () => {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {type: "check_request"})
-    })
-})
-
 document.getElementById("imaging_btn_open").addEventListener("click", () => {
     chrome.tabs.create({ url: "imaging_helper.html", active: true, index: 0 }, (tab) => {})        
 })
